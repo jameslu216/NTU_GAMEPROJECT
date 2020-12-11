1e894e2eb07c9d63ecc51b463131a9d2b02ed502
James jr <jameslu91223@gmail.com>
2020-12-11 15:47:55 +0800

    Initial commit

---
+++ Assets/Script/Unit.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unit : MonoBehaviour {
+    public string UnitName;
+    public int HP = 100;
+    public float Speed = 3;
+    public int Atk = 10;
+    public int Team = 0;
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.GetComponent<BulletMove>().Team != Team)//如果Team值不同則受到傷害
+        {
+            HP -= 10; //生命值-10
+            Destroy(col.gameObject);
+            if (HP < 0)
+            {
+                Destroy(gameObject); //摧毀物件
+            }
+        }
+    }
+}
---
+++ Assets/Script/ＭakeBullet.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections;
+
+public class MakeBullet:MonoBehaviour{
+/*
+    public GameObject obj_Bullet;
+    public float f_Time;
+    public float f_timeToShoot;
+
+    void Start()
+    {
+        f_Time=0;
+    }
+    void Update(){
+        f_Time+=f_Time.deltaTime;
+        if(Input.GetMouseButtonDown(0))
+        {
+            Fire();
+        }
+    }
+    void Fire()
+    {
+        if(f_Time>=f_timeToShoot)
+        {
+            GameObject obj_CloneBullet=Instantiate(obj_Bullet) as GameObject;
+            Vector3 Math_Point = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y,10));
+            obj_CloneBullet.transform.position = Math_Point;
+        }
+    }
+
+
+*/
+}
