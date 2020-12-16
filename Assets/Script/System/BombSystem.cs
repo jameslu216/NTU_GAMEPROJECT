c4c0ca5cd75ddccb91ce7057108b8fbd3454f151
bogay <pojay11523@gmail.com>
2020-12-16 17:47:28 +0800

    Add bomb & player systems

---
+++ Assets/Script/System/BombSystem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using BombShooting.Control;
+using BombShooting.Utils;
+using UnityEngine;
+
+namespace BombShooting.System
+{
+    public class BombSystem : Singleton<BombSystem>
+    {
+        private Player target;
+
+        void Start()
+        {
+            this.target = PlayerSystem.Instance.randomPlayer();
+        }
+
+        public void SwitchTarget(Player newTarget)
+        {
+            if(this.target == newTarget)
+            {
+                Debug.Log("The same player got the bomb!");
+                return;
+            }
+            this.target = newTarget;
+        }
+    }
+}
