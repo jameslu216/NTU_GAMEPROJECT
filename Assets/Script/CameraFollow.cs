fce20168ccdecd988f5537fe1e70884b246e3d03
bogay <pojay11523@gmail.com>
2020-12-20 21:39:00 +0800

    Add CameraFollow.cs

---
+++ Assets/Script/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using BombShooting.Control;
+using BombShooting.Utils;
+using UniRx;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    private void Start()
+    {
+        transform
+            .Follow(
+                target,
+                smooth : 0.45f,
+                lockZ : true
+            )
+            .Subscribe()
+            .AddTo(this);
+    }
+}
---
+++ Assets/Script/Utils/TransformExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+
+namespace BombShooting.Utils
+{
+    public static class TransformExtension
+    {
+        public static IObservable<Vector3> Follow(
+            this Transform self,
+            Transform other,
+            float smooth = 0.87f,
+            bool lockX = false,
+            bool lockY = false,
+            bool lockZ = false
+        )
+        {
+            Vector3 backupPosition = self.position;
+            return other
+                .ObserveEveryValueChanged(t => t.position)
+                .Do(pos =>
+                {
+                    var newPosition = Vector3.Lerp(self.position, pos, smooth);
+                    if(lockX) newPosition.x = backupPosition.x;
+                    if(lockY) newPosition.y = backupPosition.y;
+                    if(lockZ) newPosition.z = backupPosition.z;
+                    self.position = newPosition;
+                });
+        }
+    }
+}
