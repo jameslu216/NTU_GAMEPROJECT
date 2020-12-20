a8c9cb708e1830e2beea1d02d177894327ce14c9
bogay <pojay11523@gmail.com>
2020-12-20 21:15:59 +0800

    Move `PlayerSystem` to `BombShooting.System`

--- Assets/Script/System/PlayerSystem.cs
+++ Assets/Script/System/PlayerSystem.cs
@@ -4,20 +4,23 @@ using BombShooting.Control;
 using BombShooting.Utils;
 using UnityEngine;
 
-public class PlayerSystem : Singleton<PlayerSystem>
+namespace BombShooting.System
 {
-    private List<Player> players;
-    public IEnumerable<Player> Players => this.players.AsReadOnly();
-
-    private void Start()
+    public class PlayerSystem : Singleton<PlayerSystem>
     {
-        this.players = new List<Player>(GameObject.FindObjectsOfType<Player>());
-    }
+        private List<Player> players;
+        public IEnumerable<Player> Players => this.players.AsReadOnly();
 
-    public Player randomPlayer()
-    {
-        if(this.players.Count == 0)
-            return null;
-        return this.players[Random.Range(0, this.players.Count)];
+        private void Start()
+        {
+            this.players = new List<Player>(GameObject.FindObjectsOfType<Player>());
+        }
+
+        public Player randomPlayer()
+        {
+            if(this.players.Count == 0)
+                return null;
+            return this.players[Random.Range(0, this.players.Count)];
+        }
     }
 }
