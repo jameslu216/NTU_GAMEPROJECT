1840addf36a69abb5d40aaa51221aa5c14958a38
bogay <pojay11523@gmail.com>
2020-12-25 21:45:04 +0800

    Add `AudioManager`

---
+++ Assets/Script/Audio/AudioClipArray.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace BombShooting.Audio
+{
+    [CreateAssetMenu]
+    public class AudioClipArray : ScriptableObject, IEnumerable<Sound>
+    {
+        //建立一個 Sound 的陣列
+        public Sound[] sounds;
+
+        public Sound this [int i] => this.sounds[i];
+        public IEnumerator<Sound> GetEnumerator() => this.sounds.Cast<Sound>().GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+    }
+}
