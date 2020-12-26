ef22eeedc1351aed9664a7f69e6342394ffca8c0
bogay <pojay11523@gmail.com>
2020-12-27 03:03:29 +0800

    Scale mask when bomb status changing

---
+++ Assets/Script/UI/PlayerViewMask.cs
@@ -0,0 +1,26 @@
+﻿using BombShooting.Control;
+using DG.Tweening;
+using UniRx;
+using UnityEngine;
+
+namespace BombShooting.UI
+{
+    public class PlayerViewMask : MonoBehaviour
+    {
+        [SerializeField]
+        private PlayerStatus status;
+
+        protected void Start()
+        {
+            var rect = GetComponent<RectTransform>();
+            this.status.hasBomb
+                .Select(bomb => bomb ? 2 : 1)
+                .Subscribe(scalar =>
+                    rect
+                    .DOScale(Vector3.one * scalar, 1)
+                    .SetEase(Ease.InOutBounce)
+                )
+                .AddTo(this);
+        }
+    }
+}
