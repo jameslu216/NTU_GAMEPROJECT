2e928f4c73077ebeb1d97137657c5ee0b137c6b2
bogay <pojay11523@gmail.com>
2020-12-27 02:46:04 +0800

    Add mask for player canvas

--- Assets/Script/UI/Mask.cs
+++ Assets/Script/UI/Mask.cs
@@ -1,16 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.Rendering;
+using UnityEngine.UI;
 public class Mask : Image
 {
-   public override Material materialForRendering
-   {
-       get{
-           Material material = new Material(base.materialForRendering);
-           material.SetInt("_StencilComp",(int)CompareFunction.NotEqual);
-           return material;
-       }
-   }
+    public override Material materialForRendering
+    {
+        get
+        {
+            Material material = new Material(base.materialForRendering);
+            material.SetInt("_StencilComp", (int) CompareFunction.NotEqual);
+            return material;
+        }
+    }
 }
