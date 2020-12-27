3b212123345873bfb9453764acd65c6826648dfb
bogay <pojay11523@gmail.com>
2020-12-27 15:47:17 +0800

    Add StartMenu scene

---
+++ Assets/Script/UI/StartMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace BombShooting.UI
+{
+    public class StartMenu : MonoBehaviour
+    {
+        [SerializeField]
+        private Button start;
+        [SerializeField]
+        private Button exit;
+
+        void Start()
+        {
+            this.start.onClick.AddListener(() => SceneManager.LoadScene("GameScene 1"));
+            this.exit.onClick.AddListener(() => Application.Quit());
+        }
+    }
+}
