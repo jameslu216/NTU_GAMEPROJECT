a452905e4fb416b55560f338a2be9d0aaf661da4
bogay <pojay11523@gmail.com>
2020-12-27 16:40:16 +0800

    Fill black before load game scene

--- Assets/Script/UI/StartMenu.cs
+++ Assets/Script/UI/StartMenu.cs
@@ -1,7 +1,7 @@
-﻿using UnityEngine;
+﻿using DG.Tweening;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-
 namespace BombShooting.UI
 {
     public class StartMenu : MonoBehaviour
@@ -10,10 +10,17 @@ namespace BombShooting.UI
         private Button start;
         [SerializeField]
         private Button exit;
+        [SerializeField]
+        private Image black;
 
         void Start()
         {
-            this.start.onClick.AddListener(() => SceneManager.LoadScene("GameScene 1"));
+            this.start.onClick.AddListener(() =>
+            {
+                this.black
+                    .DOColor(Color.black, 0.3f)
+                    .OnComplete(() => SceneManager.LoadScene("GameScene 1"));
+            });
             this.exit.onClick.AddListener(() => Application.Quit());
         }
     }
